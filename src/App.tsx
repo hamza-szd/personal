471a897522d64c8160d191db799d0636b43e118c
Damian Reiter <damian.reiter@gmail.com>
2022-04-26 23:25:09 -0400

    add stripe gradient

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import Footer from './components/Footer/Footer'
 import Intro from './components/Intro/Intro'
 
 function App() {
+  React.useEffect(() => {
+      const event = new Event('rendered')
+      document.dispatchEvent(event)
+  }, [])
+
   return (
     <code>
+      <div className="section_background-wrap">
+        <canvas id="gradient-canvas" data-js-darken-top data-transition-in></canvas>
+      </div>
       <div className='container'>
         <Intro />
         <Footer />
